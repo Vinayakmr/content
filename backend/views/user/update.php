92e97b65dacdc62fb73a292803b2cf77ee31cff3
csiv <vinayakmr7@gmail.com.com>
2018-08-17 12:33:32 +0530

    second commit

--- backend/views/user/update.php
+++ backend/views/user/update.php
@@ -4,8 +4,8 @@ use yii\helpers\Html;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\User */
-
-$this->title = 'Update User: {nameAttribute}';
+Yii::$app->homeUrl = '?r=user/index';
+$this->title = 'Update User';
 $this->params['breadcrumbs'][] = ['label' => 'Users', 'url' => ['index']];
 $this->params['breadcrumbs'][] = ['label' => $model->id, 'url' => ['view', 'id' => $model->id]];
 $this->params['breadcrumbs'][] = 'Update';
